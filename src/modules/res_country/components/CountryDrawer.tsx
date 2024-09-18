f77b1a1373a655a7395d88a3e1f1f780a2669197
lobatoabrahan <83128157+lobatoabrahan@users.noreply.github.com>
2024-09-18 19:43:37 +0000

    fix

--- src/modules/res_country/components/CountryDrawer.tsx
+++ src/modules/res_country/components/CountryDrawer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Alert, Drawer } from 'antd';
 import useOnFormSuccess from '../../global/hooks/useOnFormSuccess';
 import { DrawerPropsTypes } from '../../global/types/DrawerProps';
-import { useCountryRealtimeById } from '../hooks/useCountryRealtimeById';
 import CountryForm from './CountryForm';
+import { useCountryRealtimeById } from '../hooks/useCountryRealtime';
 
 const CountryDrawer: React.FC<DrawerPropsTypes> = ({ isOpen, onClose, id, onSuccess }) => {
     const { country, countryIsLoading, countryIsError, countryError } = useCountryRealtimeById(id);
